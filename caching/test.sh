3c4df9d9f6a3ae3ad7224dc05810fe05cbed689f
Eric Moritz <eric@themoritzfamily.com>
2010-11-10 03:30:02 +0000

    Added test cases where the other cases are put behind Varnish

--- caching/test.sh
+++ caching/test.sh
@@ -19,7 +19,7 @@ assertequal "$(curl http://localhost:8000/mw_spouse)" "Gina Moritz"
 assertequal "$(curl http://localhost:8000/mw_children)" "Aiden Moritz,Ethan Moritz"
 
 # Test the Internal Data Caching methods
-for i in control memcache libmc simple middleware; do
+for i in control memcache libmc simple middleware varnish_indirect; do
   URI=http://localhost:8000/$i
   echo "Priming the $i cache"
   curl $URI > results/$i.out.txt
@@ -28,10 +28,10 @@ for i in control memcache libmc simple middleware; do
 done
 
 # Do the varnish testing
-for i in varnish_indirect esi; do
+for i in memcache libmc simple middleware varnish_indirect esi; do
   URI=http://localhost:10001/$i
   echo "Priming the $i cache"
-  curl $URI > results/$i.out.txt
-  assertequal "$(cat results/$i.out.txt)" "Gina Moritz;Aiden Moritz,Ethan Moritz"
-  ab -n $SAMPLE_SIZE -c1 $URI  > results/$i.ab.txt
+  curl $URI > results/varnish-$i.out.txt
+  assertequal "$(cat results/varnish-$i.out.txt)" "Gina Moritz;Aiden Moritz,Ethan Moritz"
+  ab -n $SAMPLE_SIZE -c1 $URI  > results/varnish-$i.ab.txt
 done
