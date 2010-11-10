b381325e6c5ba0f278fc5fbdb022478f3704e21d
Eric Moritz <eric@themoritzfamily.com>
2010-11-10 21:30:12 +0000

    Removed to original experiment that inspired the whole sutie

--- cachecompare/cachecompare/apps/control/fml/fmld/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-
-# Get the sample fb data
-from cachecompare import sample_fb_data
-
-
-def index(request):
-    fb_data = sample_fb_data
-    return render_to_response("index.html", {"data": fb_data})
--- cachecompare/cachecompare/apps/control/fml/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('',
-    (r'^$', 'fmld.views.index'),
-
-)
