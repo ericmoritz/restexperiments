7948ca76e3f8c841579a50848de2fa50afb90e38
root <root@experiments.(none)>
2010-11-10 04:42:55 +0000

    Added testing of the resources needed for client side collation

--- caching/test.sh
+++ caching/test.sh
@@ -10,13 +10,6 @@ function assertequal {
     fi
 }
 
-# Precache the resources
-assertequal "$(curl http://localhost:10001/spouse)" "Gina Moritz"
-assertequal "$(curl http://localhost:10001/children)" "Aiden Moritz,Ethan Moritz"
-assertequal "$(curl http://localhost:8000/spouse)" "Gina Moritz"
-assertequal "$(curl http://localhost:8000/children)" "Aiden Moritz,Ethan Moritz"
-assertequal "$(curl http://localhost:8000/mw_spouse)" "Gina Moritz"
-assertequal "$(curl http://localhost:8000/mw_children)" "Aiden Moritz,Ethan Moritz"
 
 # Test the Internal Data Caching methods
 for i in control memcache libmc simple middleware varnish_indirect; do
@@ -28,10 +21,18 @@ for i in control memcache libmc simple middleware varnish_indirect; do
 done
 
 # Do the varnish testing
-for i in memcache libmc simple middleware varnish_indirect esi; do
+for i in esi; do
   URI=http://localhost:10001/$i
   echo "Priming the $i cache"
-  curl $URI > results/varnish-$i.out.txt
-  assertequal "$(cat results/varnish-$i.out.txt)" "Gina Moritz;Aiden Moritz,Ethan Moritz"
-  ab -n $SAMPLE_SIZE -c1 $URI  > results/varnish-$i.ab.txt
+  curl $URI > results/$i.out.txt
+  assertequal "$(cat results/$i.out.txt)" "Gina Moritz;Aiden Moritz,Ethan Moritz"
+  ab -n $SAMPLE_SIZE -c1 $URI  > results/$i.ab.txt
+done
+
+# Do the client side tests (aka AJAX)
+for i in spouse children ajax.html; do
+  URI=http://localhost:10001/$i
+  echo "Priming the $i cache"
+  curl $URI > results/ajax-$i.out.btxt
+  ab -n $SAMPLE_SIZE -c1 $URI  > results/ajax-$i.ab.txt
 done
