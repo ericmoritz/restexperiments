7d94c026ef2e4aa82fdc94a0082f55d217e1fa50
root <root@experiments.(none)>
2010-11-11 03:42:05 +0000

    Removed routes Added the never_cache decorater across the board to create consistency

--- restexperiments/phase1/app.py
+++ restexperiments/phase1/app.py
@@ -1,9 +1,6 @@
-from routes import Mapper
 from restexperiments.core.controllers import FrontController
-from routes.middleware import RoutesMiddleware
+from restexperiments.core.cache import never_cache
 from apps import conventional, restful
-
-mapper = Mapper()
 
 
 def control(environ, start_response):
@@ -13,43 +10,14 @@ def control(environ, start_response):
 ###
 # Implementations
 ###
-mapper.connect("control",
-               "/control",
-               resource="control")
+application = FrontController(
+    ('/control', control),
+    ('/restful/spouse', restful.spouse),
+    ('/restful/children', restful.children),
+    ('/conventional/direct/family', conventional.direct_family),
+    ('/restful/direct/family', restful.direct_family),
+    ('/restful/indirect/family', restful.indirect_family),
+    ('/restful/esi/family', restful.esi_family),
+)
 
-mapper.connect("conventional/direct/family",
-               "/conventional/direct/family",
-               resource="conventional/direct/family")
-
-mapper.connect("restful/direct/family",
-               "/restful/direct/family",
-               resource="restful/direct/family")
-
-mapper.connect("restful/indirect/family",
-               "/restful/indirect/family",
-               resource="restful/indirect/family")
-
-mapper.connect("restful/esi/family",
-               "/restful/esi/family",
-               resource="restful/indirect/family")
-
-mapper.connect("restful/spouse",
-               "/restful/spouse",
-               resource="restful/spouse")
-
-mapper.connect("restful/children",
-               "/restful/children",
-               resource="restful/children")
-
-
-application = FrontController(**{
-        'control': control,
-        'conventional/direct/family': conventional.direct_family,
-        'restful/direct/family': restful.direct_family,
-        'restful/indirect/family': restful.indirect_family,
-        'restful/esi/family': restful.esi_family,
-        'restful/spouse': restful.spouse,
-        'restful/children': restful.children,
-})
-
-application = RoutesMiddleware(application, mapper)
+application = never_cache(application)
