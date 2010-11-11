20c7f5ca4c227c1b450f57a18aa4246fda0b340f
root <root@experiments.(none)>
2010-11-11 15:35:42 +0000

    Added a kill -hup to the previous uwsgi proc to refresh it

--- bin/start_uwsgi.sh
+++ bin/start_uwsgi.sh
@@ -1,1 +1,7 @@
-./bin/uwsgi -s sock/uwsgi.sock -i -H `pwd`/ -M -p 4 -z 30 -l 500 -L  --module restexperiments.app
+if [ -e pid/uwsgi.pid ]; then
+    echo "Existing uwsgi, killing it."
+    kill -int `cat pid/uwsgi.pid`
+    sleep 0.5
+fi
+
+./bin/uwsgi --pidfile pid/uwsgi.pid -s sock/uwsgi.sock -i -H `pwd`/ -t 5 -M -p 4 -z 30 -l 500 -L  --module restexperiments.app
