dd98948e6a5106f6a1a3b0eaf66dc3c73a33ad22
root <root@experiments.(none)>
2010-11-11 15:36:56 +0000

    Added a concurrency arg

--- bin/test.sh
+++ bin/test.sh
@@ -13,68 +13,86 @@ function benchmark {
     TESTNAME=$2
     URI=$3
     SAMPLESIZE=$4
-    EXPECT=$5
+    CONCURRENCY=$5
+    EXPECT=$6
+
     BASE="results/$EXPERIMENT/$TESTNAME"
 
-    echo "Testing $TESTNAME"
+    echo "Testing $TESTNAME $SAMPLESIZE $CONCURRENCY"
     echo "Download $URI"
+
     curl "$URI" > "$BASE.out"
 
     if [ "$EXPECT"x != "x" ]; then
         assertequal "$(cat $BASE.out)" "$EXPECT"
     fi
     echo "Benchmarking $URI"
-    ab -n "$SAMPLESIZE" -c1 "$URI" > "$BASE.ab.txt"
+    ab -n "$SAMPLESIZE" "-c$CONCURRENCY" "$URI" > "$BASE.ab.txt"
 
 }
 
 # The phase one benchmarking
 function phase1 {
     SAMPLESIZE=$1
+    CONCURRENCY=$2
 
     mkdir -p results/phase1
     rm results/phase1/*
 
-    benchmark "phase1" "control" \
-        "http://localhost:8000/phase1/control" \
-        "$SAMPLESIZE" \
-        "Gina Moritz;Aiden Moritz,Ethan Moritz"
+    echo "sample: $SAMPLESIZE concurrency: $CONCURRENCY" > results/phase1/config.txt
 
+    benchmark "phase1"\
+        "control"\
+        "http://localhost:8000/phase1/control"\
+	"$SAMPLESIZE"\
+	"$CONCURRENCY"\
+	"Gina Moritz;Aiden Moritz,Ethan Moritz"
 
-    benchmark "phase1" "spouse" \
-        "http://localhost:8000/phase1/restful/spouse" \
-        "$SAMPLESIZE" \
+    benchmark "phase1"\
+	"spouse"\
+        "http://localhost:8000/phase1/restful/spouse"\
+        "$SAMPLESIZE"\
+	"$CONCURRENCY"\
         "Gina Moritz"
 
-    benchmark "phase1" "children" \
-        "http://localhost:8000/phase1/restful/children" \
-        "$SAMPLESIZE" \
+    benchmark "phase1"\
+	"children"\
+        "http://localhost:8000/phase1/restful/children"\
+        "$SAMPLESIZE"\
+	"$CONCURRENCY"\
         "Aiden Moritz,Ethan Moritz"
 
-
-
-    benchmark "phase1" "traditional-direct" \
-        "http://localhost:8000/phase1/conventional/direct/family" \
-        "$SAMPLESIZE" \
+    benchmark "phase1"\
+	"traditional-direct"\
+        "http://localhost:8000/phase1/conventional/direct/family"\
+        "$SAMPLESIZE"\
+	"$CONCURRENCY"\
         "Gina Moritz;Aiden Moritz,Ethan Moritz"
 
-    benchmark "phase1" "restful-direct" \
-        "http://localhost:8000/phase1/restful/direct/family" \
-        "$SAMPLESIZE" \
+    benchmark "phase1"\
+        "restful-direct"\
+        "http://localhost:8000/phase1/restful/direct/family"\
+        "$SAMPLESIZE"\
+	"$CONCURRENCY"\
         "Gina Moritz;Aiden Moritz,Ethan Moritz"
 
-    benchmark "phase1" "restful-indirect" \
-        "http://localhost:8000/phase1/restful/indirect/family" \
-        "$SAMPLESIZE" \
+    benchmark "phase1"\
+        "restful-indirect"\
+        "http://localhost:8000/phase1/restful/indirect/family"\
+        "$SAMPLESIZE"\
+	"$CONCURRENCY"\
         "Gina Moritz;Aiden Moritz,Ethan Moritz"
 
-    benchmark "phase1" "restful-esi" \
-        "http://localhost:10001/phase1/restful/esi/family" \
-        "$SAMPLESIZE" \
+    benchmark "phase1"\
+	"restful-esi"\
+        "http://localhost:10001/phase1/restful/esi/family"\
+        "$SAMPLESIZE"\
+	"$CONCURRENCY"\
         "Gina Moritz;Aiden Moritz,Ethan Moritz"
 
 }
 
 SAMPLESIZE=$1
+CONCURRENCY=$2
 
-phase1 $SAMPLESIZE
+phase1 $SAMPLESIZE $CONCURRENCY
