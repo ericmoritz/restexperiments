9a41f1c64aa1bc48ce5a0c0d1b3f13aa2f16a3d1
root <root@experiments.(none)>
2010-11-11 03:34:39 +0000

    Replaced routes with a more consistent router

--- restexperiments/app.py
+++ restexperiments/app.py
@@ -1,14 +1,9 @@
 from core.controllers import FrontController, serve
-from routes import Mapper
-from routes.middleware import RoutesMiddleware
 from phase1.app import application as phase1
 
-mapper = Mapper()
-mapper.connect("phase1", "/phase1{path_info:.*}",
-               resource="phase1")
-
-application = FrontController(phase1=phase1)
-application = RoutesMiddleware(application, mapper)
+application = FrontController(
+    ("/phase1", phase1),
+)
 
 if __name__ == '__main__':
     serve(application)
