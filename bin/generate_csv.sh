a1aa1d35b8c8c7ba45ffe717b2e680207462424c
Eric Moritz <eric@themoritzfamily.com>
2010-11-17 00:54:16 +0000

    Moved the csv files to the _static/data director

--- bin/generate_csv.sh
+++ bin/generate_csv.sh
@@ -1,10 +1,12 @@
+
 for PHASE in "phase1"; do
+    mkdir -p docs/source/_static/data/$PHASE/
     for i in data/$PHASE/*/; do
         python bin/report.py $i
-    done | python bin/report_to_csv.py tpr > docs/source/data/phase1/tpr.csv 
+    done | python bin/report_to_csv.py tpr > docs/source/_static/data/$PHASE/tpr.csv 
     
     
     for i in data/$PHASE/*/;
      do python bin/report.py $i; 
-    done | python bin/report_to_csv.py rps > docs/source/data/phase1/rps.csv 
+    done | python bin/report_to_csv.py rps > docs/source/_static/data/$PHASE/rps.csv 
 done
