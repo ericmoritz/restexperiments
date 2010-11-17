42053b6d3151b9ed589f216850e7028c63fe48f2
Eric Moritz <eric@themoritzfamily.com>
2010-11-17 03:39:46 +0000

    Mount the /phase1 app at /phase2 and added latency to the db calls

--- restexperiments/app.py
+++ restexperiments/app.py
@@ -1,8 +1,46 @@
 from core.controllers import FrontController, serve
-from phase1.app import application as phase1
+from core import db
+from phase1.app import application as base_app
+import time
+
+
+def add_latency(secs):
+    def decor(func):
+        def inner(*args, **kwargs):
+            time.sleep(secs)
+            return func(*args, **kwargs)
+        return inner
+    return decor
+
+# This middleware adds latency to the database calls
+class LatencyConfigMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['get_spouse'] = add_latency(0.01)(db.get_spouse)
+        environ['get_children'] = add_latency(0.01)(db.get_children)
+    
+        return self.app(environ, start_response)
+
+# This middleware allows the database calls to be referenced in the
+# WSGI environment
+class ConfigMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['get_spouse'] = db.get_spouse
+        environ['get_children'] = db.get_children
+
+        return self.app(environ, start_response)
+
+phase1 = ConfigMiddleware(base_app)
+phase2 = LatencyConfigMiddleware(base_app)
 
 application = FrontController(
     ("/phase1", phase1),
+    ("/phase2", phase2),
 )
 
 if __name__ == '__main__':
