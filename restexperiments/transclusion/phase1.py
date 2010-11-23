baef614dd0dbc24e75e0e18b4636358ab6997e3f
root <root@experiments.(none)>
2010-11-23 02:44:33 +0000

    Fixed the formatting of the direct family resource

--- restexperiments/transclusion/phase1.py
+++ restexperiments/transclusion/phase1.py
@@ -31,7 +31,7 @@ def direct_family(environ, start_response):
     spouse = environ['get_spouse']()
     children = environ['get_children']()
     start_response("200 OK", [("Content-Type", "text/plain")])
-    return ["%s;%s" % (spouse, children)]
+    return ["%s;%s" % (spouse, ",".join(children))]
 
 
 def indirect_family(environ, start_response):
